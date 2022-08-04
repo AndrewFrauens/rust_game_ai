4511dffc37c1751e5944e58535f8204beafee1ac
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2022-08-04 05:52:01 -0500

    Modified greet_people system to work off a timer resource

--- src/bin/bevy_hello_world.rs
+++ src/bin/bevy_hello_world.rs
@@ -32,9 +32,15 @@ fn add_people(mut commands: Commands) {
         .insert(Name("King George".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello, {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // Update our timer with the time since the last  update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello, {}!", name.0);
+        }
     }
 }
 
@@ -42,8 +48,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
